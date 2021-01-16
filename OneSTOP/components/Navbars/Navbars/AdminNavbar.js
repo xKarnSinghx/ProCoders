cac41d7c741536fe20e7d90c57d5ab4cea3ac93c
Mohd Zubair <55323065+Mohdzubair3056@users.noreply.github.com>
2021-01-16 13:14:24 +0530

    Add files via upload

---
+++ OneSTOP/components/Navbars/Navbars/AdminNavbar.js
@@ -0,0 +1,63 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Navbar,
+  Nav,
+  Container,
+  Media
+} from "reactstrap";
+
+class AdminNavbar extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+          <Container fluid>
+            <Link
+              className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
+              to="/"
+            >
+              {this.props.brandText}
+            </Link>
+            
+            <Nav className="align-items-center d-none d-md-flex" navbar>
+              <UncontrolledDropdown nav>
+                <DropdownToggle className="pr-0" nav>
+                  <Media className="align-items-center">
+                    <Media className="ni ni-settings-gear-65"/>
+                  </Media>
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu-arrow" right>
+                  <DropdownItem className="noti-title" header tag="div">
+                    <h6 className="text-overflow m-0">Welcome!</h6>
+                  </DropdownItem>
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                    <i className="ni ni-single-02" />
+                    <span>My profile</span>
+                  </DropdownItem>
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                    <i className="ni ni-settings-gear-65" />
+                    <span>Settings</span>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
+                    <i className="ni ni-user-run" />
+                    <span>Logout</span>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default AdminNavbar;
---
+++ OneSTOP/components/Navbars/Navbars/AuthNavbar.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  UncontrolledCollapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+class AdminNavbar extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar
+          className="navbar-top navbar-horizontal navbar-dark"
+          expand="md"
+        >
+          <Container className="px-4">
+            <NavbarBrand to="/" tag={Link}>
+            <span className="nav-link-inner--text">One Stop</span>
+            </NavbarBrand>
+            <button className="navbar-toggler" id="navbar-collapse-main">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <UncontrolledCollapse navbar toggler="#navbar-collapse-main">
+              <div className="navbar-collapse-header d-md-none">
+                <Row>
+                  <Col className="collapse-brand" xs="6">
+                    <Link to="/">
+                    <h1 className="nav-link-inner--text">One Stop</h1>
+                    </Link>
+                  </Col>
+                  <Col className="collapse-close" xs="6">
+                    <button
+                      className="navbar-toggler"
+                      id="navbar-collapse-main"
+                    >
+                      <span />
+                      <span />
+                    </button>
+                  </Col>
+                </Row>
+              </div>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink className="nav-link-icon" to="/" tag={Link}>
+                    <i className="ni ni-planet" />
+                    <span className="nav-link-inner--text">Home</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className="nav-link-icon"
+                    to="/auth/register"
+                    tag={Link}
+                  >
+                    <i className="ni ni-circle-08" />
+                    <span className="nav-link-inner--text">Register</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className="nav-link-icon"
+                    to="/auth/login"
+                    tag={Link}
+                  >
+                    <i className="ni ni-key-25" />
+                    <span className="nav-link-inner--text">Login</span>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </UncontrolledCollapse>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default AdminNavbar;
