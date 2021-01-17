6489a28a66091cec2122dec0a5a2090461f83169
Mohdzubair3056 <55323065+Mohdzubair3056@users.noreply.github.com>
2021-01-17 22:59:58 +0530

    updated

--- OneSTOP/src/views/examples/UpcomingContests.js
+++ OneSTOP/src/views/examples/UpcomingContests.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "components/Headers/Header.js";
-import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
 import App from "../../components/App";
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -10,42 +9,25 @@ import { store } from '../../store/store';
 
 
 // reactstrap components
-import {
-  Button,
-  CardHeader,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  
-} from "reactstrap";
 
 export default function Contest(){
+
   const Main = withRouter((props) => <App {...props} />)
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Main />
-        </BrowserRouter>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
   reportWebVitals();
-  return (
+  return(
+  ReactDOM.render(
     <>
-      <div className="header bg-gradient-info pb-4 pt-5 pt-md-8">
-          <Container fluid>
-            <div className="header-body">
-              <h1>Find All The Upcoming Contests Here</h1>
-              <Row>
-              </Row>
-            </div>
-          </Container>
-      </div>
-    </>
+      <React.StrictMode>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Main />
+          </BrowserRouter>
+        </Provider>
+      </React.StrictMode>
+    </>,
+    document.getElementById('root'),
+  )
   );
+  
+  
 }
